0f2d17917cbd2fb84365d0aa0db5f122f3396c85
Marianela Holsbach <marianelaholsbach@gmail.com>
2022-09-12 14:54:13 -0300

    arreglando html

--- main.js
+++ main.js
@@ -7,20 +7,20 @@
       this.img = img;
     }
   }  */
-  class Paciente {
-    constructor(Nombre, Apellido, Dia, Orden, telefono, doctor) {
-      this.Nombre = Nombre;
-      this.Apellido = Apellido;
-      this.Dia = Dia;
-      this.Orden = Orden;
-      this.telefono = telefono;
-      this.doctor = doctor;
-    }
+class Paciente {
+  constructor(Nombre, Apellido, Dia, Orden, telefono, doctor) {
+    this.Nombre = Nombre;
+    this.Apellido = Apellido;
+    this.Dia = Dia;
+    this.Orden = Orden;
+    this.telefono = telefono;
+    this.doctor = doctor;
   }
-  
-  //TABLA DE DOCTORES
-  
-  /*   let lunes = new Horarios(
+}
+
+//TABLA DE DOCTORES
+
+/*   let lunes = new Horarios(
       "Lunes",
       "Dr. Lautaro Gimenez",
       "Clinico",
@@ -63,108 +63,107 @@
     dr.push(martes2);
     dr.push(mier);
     dr.push(vier);  */
-  
-  //MANIPULACION DE DOM
-  
-  async function traerDoctores() {
-    let res = await fetch("datos.json");
-    let json = await res.json();
-  
-    console.log(json);
-    let html = "";
-    json.forEach((tarjetaDr) => {
-      html += `
-          <div class="card" style="width: 13rem;" align="center">
+
+//MANIPULACION DE DOM
+
+async function traerDoctores() {
+  let res = await fetch("datos.json");
+  let json = await res.json();
+
+  console.log(json);
+  let html = "";
+  json.forEach((tarjetaDr) => {
+    html += `
+          <div class="card" >
               <h3 class="title" align="center">${tarjetaDr.nombre}</h3>
               <img class="card-img-top" style= "height: 7rem;" src=${tarjetaDr.img}>
               <div class="card-body">            
                 
                 
                 <p class="card-text">${tarjetaDr.especialidad}</p>
-                <p class="card-text">${tarjetaDr.prepaga}</p>
+                
                
               </div>
           `;
-    });
-    document.getElementById("card-doctores").innerHTML = html;
-  }
-  traerDoctores();
-  
-  // LOCAL STORAGE
-  let arrayFormulario = [];
-  
-  //OPTIMIZANDO
-  
-  localStorage.getItem("arrayFormulario")
-    ? (arrayFormulario = JSON.parse(localStorage.getItem("arrayFormulario")))
-    : localStorage.setItem("arrayFormulario", JSON.stringify(arrayFormulario));
-  
-  //TABLA DE TURNOS
-  //USO DE EVENTOS
-  
-  formRegistro.addEventListener("submit", (e) => {
-    e.preventDefault();
-  
-    let Nombre = document.getElementById("nombres").value;
-    let Apellido = document.getElementById("apellidos").value;
-    let Dia = document.getElementById("dia").value;
-    let Orden = document.getElementById("orden").value;
-    let telefono = document.getElementById("telefono").value;
-    let doctor = document.getElementById("doctor").value;
-    const formulario = new Paciente(
-      Nombre,
-      Apellido,
-      Dia,
-      Orden,
-      telefono,
-      doctor
-    );
-    arrayFormulario.push(formulario);
-    localStorage.setItem("arrayFormulario", JSON.stringify(arrayFormulario));
-    formRegistro.reset();
-    Swal.fire(
-      "Turno Registrado",
-      `Hola ${Nombre}! Has registrado tu turno con exito.`
-    );
   });
-  
-  const verTurno = document.getElementById("verTurno");
-  const turnos = document.getElementById("turnos");
-  verTurno.addEventListener("click", () => {
-    turnos.innerHTML = "";
-    const tarjetaTurno = JSON.parse(localStorage.getItem("arrayFormulario"));
-    tarjetaTurno.forEach((elementoTurno) => {
-      Swal.fire(
-        `${elementoTurno.Nombre} ${elementoTurno.Apellido}`,
-        `Turno el dia ${elementoTurno.Dia} a las ${elementoTurno.Orden}, con el doctor: ${elementoTurno.doctor}`
-      );
-      /* turnos.innerHTML += `
+  document.getElementById("card-doctores").innerHTML = html;
+}
+traerDoctores();
+
+// LOCAL STORAGE
+let arrayFormulario = [];
+
+//OPTIMIZANDO
+
+localStorage.getItem("arrayFormulario") ?
+  (arrayFormulario = JSON.parse(localStorage.getItem("arrayFormulario"))) :
+  localStorage.setItem("arrayFormulario", JSON.stringify(arrayFormulario));
+
+//TABLA DE TURNOS
+//USO DE EVENTOS
+
+formRegistro.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let Nombre = document.getElementById("nombres").value;
+  let Apellido = document.getElementById("apellidos").value;
+  let Dia = document.getElementById("dia").value;
+  let Orden = document.getElementById("orden").value;
+  let telefono = document.getElementById("telefono").value;
+  let doctor = document.getElementById("doctor").value;
+  const formulario = new Paciente(
+    Nombre,
+    Apellido,
+    Dia,
+    Orden,
+    telefono,
+    doctor
+  );
+  arrayFormulario.push(formulario);
+  localStorage.setItem("arrayFormulario", JSON.stringify(arrayFormulario));
+  formRegistro.reset();
+  Swal.fire(
+    "Turno Registrado",
+    `Hola ${Nombre}! Has registrado tu turno con exito.`
+  );
+});
+
+const verTurno = document.getElementById("verTurno");
+const turnos = document.getElementById("turnos");
+verTurno.addEventListener("click", () => {
+  turnos.innerHTML = "";
+  const tarjetaTurno = JSON.parse(localStorage.getItem("arrayFormulario"));
+  tarjetaTurno.forEach((elementoTurno) => {
+    Swal.fire(
+      `${elementoTurno.Nombre} ${elementoTurno.Apellido}`,
+      `Turno el dia ${elementoTurno.Dia} a las ${elementoTurno.Orden}, con el doctor: ${elementoTurno.doctor}`
+    );
+    /* turnos.innerHTML += `
              <div align="center">
                  <p><b>${elementoTurno.Nombre} ${elementoTurno.Apellido}, turno el dia ${elementoTurno.Dia}, a las ${elementoTurno.Orden}, con el doctor: ${elementoTurno.doctor} </b></p>                            
                               
                                
               </div>
              `; */
-    });
   });
-  
-  //DOCTOR DISPONIBLE EN CLINICA
-  
-  async function findEspecialidad() {
-    let res = await fetch("datos.json");
-    let json = await res.json();
-  
-    let drDispo = json.find((e) => e.especialidad == "Clinico");
-    //const clinico = document.getElementById("noticias");
-    const arrayClinico = [];
-    arrayClinico.push(drDispo);
-    console.log(arrayClinico);
-  
-    let html = "";
-  
-    arrayClinico.forEach((clinic) => {
-      html += 
-      `<div class = "personal-clinica">
+});
+
+//DOCTOR DISPONIBLE EN CLINICA
+
+/* async function findEspecialidad() {
+  let res = await fetch("datos.json");
+  let json = await res.json();
+
+  let drDispo = json.find((e) => e.especialidad == "Clinico");
+
+  const arrayClinico = [];
+  arrayClinico.push(drDispo);
+  console.log(arrayClinico);
+
+  let html = "";
+
+  arrayClinico.forEach((clinic) => {
+    html += `<div class = "personal-clinica">
           <p>Personal de clinica Actual:</p>
                 
             <h3>${clinic.nombre}</h3>                       
@@ -174,47 +173,68 @@
         </div>
                  
         `;
-    });
-    document.getElementById("noticias").innerHTML = html;
-  }
-  findEspecialidad();
-  
-  //PERSONAL QUE TRABAJE CON OBRA SOCIAL
-  
-  async function filtroPrepaga() {
-    let res = await fetch("datos.json");
-    let json = await res.json();
-    //console.log();
-  
-    let prep = json.filter((e) => e.prepaga == "SI");
-    console.log(prep);
-  
-    //const prepaga = document.getElementById("noticias");
-  
+  });
+  console.log(html);
+  document.getElementById("noticias").innerHTML = html;
+}
+findEspecialidad(); */
+
+fetch("datos.json")
+  .then((res) => res.json())
+
+  .then((datos) => {
+
+    let drDispo = datos.find((e) => e.especialidad == "Clinico");
+    console.log(drDispo)
+    const arrayClinic = [];
+
+    arrayClinic.push(drDispo);
+
+    console.log(arrayClinic);
     let html = "";
-  
+
+    arrayClinic.forEach((clinic) => {
+      html += `<div class = "personal-clinica">
+          <p>Personal de clinica Actual</p>
+          <img class="card-img-top" style= "width: 7rem;" src=${clinic.img}>    
+            <h3>${clinic.nombre}</h3>                       
+            <p class="card-text">${clinic.especialidad}</p>
+            <p class="card-text">Prepaga: ${clinic.prepaga}</p>
+                  
+        </div>
+                 
+        `;
+    });
+    console.log(html);
+    document.getElementById("noticias").innerHTML = html;
+  })
+
+//PERSONAL QUE TRABAJE CON OBRA SOCIAL
+
+ fetch("datos.json")
+  .then((res) => res.json())
+
+  .then((datos) => {
+
+    let prep = datos.filter((e) => e.prepaga == "SI");
+    console.log(prep);
+
+    let html = '<p class = "personal-pre1">Personal con Prepaga Actual</p>';
+
     prep.forEach((pre) => {
       html += `
-      <p div class = "personal-pre1">Personal con Prepaga Actual:</p> 
-      <div class = "personal-pre">
-                
-        <div class="card" style="width: 13rem;" align="center">
-            <h3 class="title" align="center">${pre.nombre}</h3>
-      
-            <div class="card-body">            
-        
-        
-                <p class="card-text">${pre.especialidad}</p>
-        
        
-        </div>
-           
+      <div class = "personal-pre">                
+        <div class="card" style="width: 13rem;">
+        <img class="card-img-top" style= "height: 7rem;" src=${pre.img}>
+            <h3 class="title" align="center">${pre.nombre}</h3>      
+            <div class="card-body">         
+                <p class="card-text">${pre.especialidad}</p>       
+            </div>          
           
       </div>
          
              `;
     });
-    document.getElementById("noticias").innerHTML = html;
-  }
-  filtroPrepaga();
-  
+    document.getElementById("noticias2").innerHTML = html;
+  }) 
